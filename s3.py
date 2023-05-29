c46cb34bf26c0d9b04d26013fb6278db92374303
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-29 21:59:45 +0330

    sadeghi

--- s3.py
+++ s3.py
@@ -6,9 +6,17 @@ window.register_shape("strawberry.gif")
 pen = turtle.Turtle()
 # pen.shape("t1") # 'arrow', 'turtle', 'circle', 'square', 'triangle', 'classic'
 pen.shape("strawberry.gif")
+pen.pencolor("green")
 
-
-
-
+pen.pensize(3)
+COLORS = ["green", "black", "purple", "cyan"]
+for j in range(4):
+    pen.fillcolor(COLORS[j])
+    pen.begin_fill()
+    for i in range(4):
+        pen.forward(100)
+        pen.stamp()
+        pen.left(90)
+    pen.end_fill()
+    pen.right(90)
 turtle.done()
-
