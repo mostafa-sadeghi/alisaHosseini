10a591d001a356b1a0d5ac0c8866dc2631ae79e1
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-29 21:18:20 +0330

    sadeghi

---
+++ s3.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.Screen()
+# window.register_shape("t1", ((5,-3),(0,5),(-5,-3)))
+window.register_shape("strawberry.gif")
+pen = turtle.Turtle()
+# pen.shape("t1") # 'arrow', 'turtle', 'circle', 'square', 'triangle', 'classic'
+pen.shape("strawberry.gif")
+
+
+
+
+turtle.done()
+
