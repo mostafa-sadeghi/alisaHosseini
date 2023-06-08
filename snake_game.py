27fd56e4d0f5826b288f2d8ecc2fa4efe076dde2
msa <msai.sadehi@gmail.com>
2023-06-08 10:01:25 +0330

    snake game

---
+++ snake_game.py
@@ -0,0 +1,62 @@
+# def hello(name):
+#     if name == "arman":
+#         print(f"hello {name}")
+#         print("welcome")
+#     elif name == "amin":
+#         print("I know you")
+#     else:
+#         print("Who are you?????")
+
+
+# name = input('Enter your name:> ')
+# hello(name)
+
+
+# def find_age_group(name, age):
+#     if age < 8:
+#         return f'{name} is kid'
+#     elif age >= 8 and age < 13:
+#         return f'{name} is junior'
+#     elif age >= 13 and age < 18:
+#         return f'{name} is teenager'
+#     else:
+#         return f'{name} is adult'
+
+# message = find_age_group("sara",11)
+# print(message)
+
+# def add_numbers(number1, number2, number3):
+
+#     return number1 + number2 + number3
+
+
+
+# result = add_numbers(3,5,8)
+# print(result)
+# تابع بالا را به این صورت تغییر بده که فقط حاصلجمع اعداد فرد را محاسبه کند و تحویل دهد
+
+
+numbers = [1,4,6,7,90,45]
+
+# def add_even_numbers(mylist):
+#     total = 0
+#     for n in mylist:
+#         if n % 2 == 0:
+#             total += n
+
+#     return total
+
+# result = add_even_numbers(numbers)
+# print(result)
+
+def find_even_numbers(mylist):
+    even_numbers = []
+    for n in mylist:
+        if n % 2 == 0:
+            even_numbers.append(n)
+
+    return even_numbers
+
+result = find_even_numbers(numbers)
+print(result)
+
